949fcaf4e24ac0f403a538cd2298fe500a828900
Aurora Loprino <aurora.loprino@studio.unibo.it>
2025-12-27 18:25:58 +0100

    I just put files in the folders

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,22 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CalendarioComponent from '../components/CalendarioComponent.vue'
-import LoginView from '../views/LoginView.vue'
+import LoginView from '../views/Login.vue'
 import RegisterView from '../views/RegisterView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import { currentUser } from '../auth/auth'
 
-
 const routes = [
-  {
-    path: '/',
-    redirect: '/calendario'
-  },
-  {
-    path: '/calendario',
-    name: 'Calendario',
-    component: CalendarioComponent
-  }
-  { path:'/', component:CalendarioComponent },
+  { path:'/', redirect: '/login' },
+  { path:'/calendario', component:CalendarioComponent },
   { path:'/login', component:LoginView },
   { path:'/register', component:RegisterView },
   { path:'/dashboard', component:DashboardView }
@@ -27,10 +18,9 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to,_,next)=>{
  if(to.path==='/dashboard' && !currentUser()) next('/login')
  else next()
 })
-
 
 export default router
