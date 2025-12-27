071f12dedaae8387e30cd269ff98f99afc6c07ca
lorenzobilli2005 <lorenzo.billi@studio.unibo.it>
2025-12-27 17:07:37 +0100

    Update index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CalendarioComponent from '../components/CalendarioComponent.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import { currentUser } from '../auth/auth'
+
 
 const routes = [
   {
@@ -11,11 +16,21 @@ const routes = [
     name: 'Calendario',
     component: CalendarioComponent
   }
+  { path:'/', component:CalendarioComponent },
+  { path:'/login', component:LoginView },
+  { path:'/register', component:RegisterView },
+  { path:'/dashboard', component:DashboardView }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to,from,next)=>{
+ if(to.path==='/dashboard' && !currentUser()) next('/login')
+ else next()
+})
+
 
 export default router
