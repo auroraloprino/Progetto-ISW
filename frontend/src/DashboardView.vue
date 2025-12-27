2ab03a5fe2b95ee0d064f56a90847ac46e8e1775
lorenzobilli2005 <lorenzo.billi@studio.unibo.it>
2025-12-27 17:26:51 +0100

    Create DashboardView.vue

---
+++ frontend/src/DashboardView.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="auth-wrapper">
+ <div class="auth-card">
+  <h2>Area privata</h2>
+  <p>Benvenuto {{user?.email}}</p>
+  <button @click="doLogout">Logout</button>
+ </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import { currentUser, logout } from '../auth/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const user = currentUser()
+
+function doLogout(){
+ logout()
+ router.push('/login')
+}
+</script>
